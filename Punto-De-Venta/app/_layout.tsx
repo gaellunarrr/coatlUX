05990187d3f5901127f5cca7e89f6f39678c118e
Gael Luna <gaellunarr@gmail.com>
2026-01-27 09:54:46 -0600

    chore: add SafeAreaProvider to root layout

--- Punto-De-Venta/app/_layout.tsx
+++ Punto-De-Venta/app/_layout.tsx
@@ -1,10 +1,13 @@
 import { Stack } from "expo-router";
 import "../global.css";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-    </Stack>
+    <SafeAreaProvider>
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      </Stack>
+    </SafeAreaProvider>
   );
 }
